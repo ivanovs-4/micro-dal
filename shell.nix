914e59b1cedf3c17f0e2d81eb5963f4207d6a68c
Sergey Ivanov <ivanovs-4@yandex.ru>
2020-05-12 04:13:06 +0400

    wip

--- shell.nix
+++ shell.nix
@@ -6,5 +6,25 @@ in pkgs.haskellPackages.shellFor {
     cabal-install
     ghcid
   ];
+  buildInputs = with pkgs; [
+                      minio
+                    ];
   packages = _: pkgs.lib.attrValues release.packages;
+
+  shellHook = ''
+      export LANG=C
+      export LC_ALL=C
+      function cleanup {
+        kill 0
+      }
+      # https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits
+      trap exit INT TERM
+      trap cleanup EXIT
+
+      export MINIO_ACCESS_KEY="s3-access-key"
+      export MINIO_SECRET_KEY="s3-secret-key"
+      minio --compat server --address :9001 ./storage &
+      sleep 1
+    '';
+
 }
--- src/Data/DAL/KeyValue/S3.hs
+++ src/Data/DAL/KeyValue/S3.hs
@@ -1,98 +1,109 @@
 {-# LANGUAGE QuasiQuotes, ExtendedDefaultRules #-}
+{-# LANGUAGE RecordWildCards #-}
 module Data.DAL.KeyValue.S3
 ( S3Engine
 , S3EngineOpts
 , createEngine
-, closeEngine
+-- , closeEngine
 , withEngine
-, optInMemory
+, s3EngineOptsDev
 ) where
 
 import Control.Applicative ((<|>))
 import Control.Exception
-import Database.S3.Simple hiding (withTransaction)
 import Data.ByteString (ByteString)
 import Data.Either
+import Data.Function
 import Data.Maybe
 import Data.Monoid
 import Data.Store
 import Data.String (IsString(..))
-import qualified Database.S3.Simple as S3
+import Data.Text (Text)
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
 import Safe
 import Text.InterpolatedString.Perl6 (qq,qc)
 
+import qualified Network.Minio as Minio
+import qualified Data.Text as T
+
 import Data.DAL.Types
 
-newtype S3Engine = S3Engine { conn :: Connection }
-
-data S3EngineOpts = S3EngineOpts
-                        { dbName :: Maybe FilePath
+data S3Engine = S3Engine
+                        { conn :: Minio.MinioConn
+                        , pref :: Text
                         }
 
-instance Monoid S3EngineOpts where
-  mempty = S3EngineOpts { dbName = Nothing
-                            }
+data S3EngineOpts = S3EngineOpts
+                        { s3Addr         :: Text
+                        , s3AccessKey    :: Text
+                        , s3SecretKey    :: Text
+                        , s3BucketPrefix :: Text
+                        }
 
-instance Semigroup S3EngineOpts where
-  (<>) a b = S3EngineOpts { dbName = dbName b <|> dbName a }
-
-instance IsString S3EngineOpts where
-  fromString s = S3EngineOpts { dbName = pure s }
-
-optInMemory :: S3EngineOpts
-optInMemory = mempty
+s3EngineOptsDev :: S3EngineOpts
+s3EngineOptsDev = S3EngineOpts
+                        { s3Addr         = "http://127.0.0.1:9001"
+                        , s3AccessKey    = "s3-access-key"
+                        , s3SecretKey    = "s3-secret-key"
+                        , s3BucketPrefix = "dev/"
+                        }
 
 createEngine :: S3EngineOpts -> IO S3Engine
-createEngine opts = do
-  conn <- open (fromMaybe ":memory:" $ dbName opts)
-  -- FIXME: move this stuff to options
-  execute_ conn "PRAGMA journal_mode = WAL"
-  execute_ conn "PRAGMA synchronous = NORMAL"
-  pure $ S3Engine conn
-
-closeEngine :: S3Engine -> IO ()
-closeEngine e = close (conn e)
+createEngine S3EngineOpts {..} = do
+  manager <- newManager tlsManagerSettings
+  let minioConnectionInfo = toAddr s3Addr & Minio.setCreds (Minio.Credentials s3AccessKey s3SecretKey)
+  minioConn <- Minio.mkMinioConn minioConnectionInfo manager
+  pure $ S3Engine minioConn s3BucketPrefix
+  where
+    toAddr = fromString . T.unpack
 
 withEngine :: S3EngineOpts -> (S3Engine -> IO a) -> IO a
 withEngine opts = bracket (createEngine opts) closeEngine
+  where
+    closeEngine :: S3Engine -> IO ()
+    closeEngine _ = pure ()
 
 instance (Store a, HasKey a) => SourceListAll a IO S3Engine where
   listAll :: S3Engine -> IO [a]
   listAll e = do
-    execute_ (conn e) [qc|create table if not exists {table} (k blob primary key, v blob)|]
-    rows <- query_ (conn e) [qc|select v from {table}|] :: IO [Only ByteString]
-    pure $ rights $ fmap (\(Only x) -> decode @a x) rows
-    where
-      table = nsUnpack (ns @a)
+    undefined
+    -- execute_ (conn e) [qc|create table if not exists {table} (k blob primary key, v blob)|]
+    -- rows <- query_ (conn e) [qc|select v from {table}|] :: IO [Only ByteString]
+    -- pure $ rights $ fmap (\(Only x) -> decode @a x) rows
+    -- where
+    --   table = nsUnpack (ns @a)
 
 instance (Store a, Store (KeyOf a), HasKey a) => SourceStore a IO S3Engine where
   load :: S3Engine -> KeyOf a -> IO (Maybe a)
   load e k = do
-    execute_ (conn e) [qc|create table if not exists {table} (k blob primary key, v blob)|]
-    bs <- query (conn e) [qc|select v from {table} where k = ?|] (Only (encode k)) :: IO [Only ByteString]
-    case headMay bs of
-      Just (Only v) -> pure $ either (const Nothing) (Just) (decode @a v)
-      _             -> pure Nothing
-    where
-      table = nsUnpack (ns @a)
+    undefined
+    -- execute_ (conn e) [qc|create table if not exists {table} (k blob primary key, v blob)|]
+    -- bs <- query (conn e) [qc|select v from {table} where k = ?|] (Only (encode k)) :: IO [Only ByteString]
+    -- case headMay bs of
+    --   Just (Only v) -> pure $ either (const Nothing) (Just) (decode @a v)
+    --   _             -> pure Nothing
+    -- where
+    --   table = nsUnpack (ns @a)
 
   store :: S3Engine -> a -> IO (KeyOf a)
   store e v = do
-    execute_ (conn e) [qc|create table if not exists {table} (k blob primary key, v blob)|]
-    execute (conn e)  [qc|insert or replace into {table} (k,v) values(?,?)|] (bkey,bval)
-    pure (key v)
-    where
-      table = nsUnpack (ns @a)
-      bkey  = encode (key v)
-      bval  = encode v
+    undefined
+    -- execute_ (conn e) [qc|create table if not exists {table} (k blob primary key, v blob)|]
+    -- execute (conn e)  [qc|insert or replace into {table} (k,v) values(?,?)|] (bkey,bval)
+    -- pure (key v)
+    -- where
+    --   table = nsUnpack (ns @a)
+    --   bkey  = encode (key v)
+    --   bval  = encode v
 
 instance (Store a, Store (KeyOf a), HasKey a) => SourceDeleteByKey a IO S3Engine where
   delete :: S3Engine -> KeyOf a -> IO ()
   delete e k = do
-    execute (conn e) [qc|delete from {table} where k  = ?|] (Only (encode k))
-    where
-      table = nsUnpack (ns @a)
+    undefined
+    -- execute (conn e) [qc|delete from {table} where k  = ?|] (Only (encode k))
+    -- where
+    --   table = nsUnpack (ns @a)
 
 instance SourceTransaction a IO S3Engine where
-  withTransaction e = S3.withTransaction (conn e)
-
+  withTransaction e = error "Is ot available"  -- FIXME implement as void or smth
