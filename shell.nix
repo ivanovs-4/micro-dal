47218756923140821a6bc86c6066be53f6aa35c5
Sergey Ivanov <ivanovs-4@yandex.ru>
2020-05-19 14:23:18 +0400

    Get back MINIO_ACCESS_KEY, MINIO_SECRET_KEY to shell.nix

--- shell.nix
+++ shell.nix
@@ -21,9 +21,12 @@ in pkgs.haskellPackages.shellFor {
       trap exit INT TERM
       trap cleanup EXIT
 
+      export MINIO_ACCESS_KEY="s3-access-key"
+      export MINIO_SECRET_KEY="s3-secret-key"
+
       export TEST_S3_ADDR="http://127.0.0.1:9001"
-      export TEST_S3_ACCESS_KEY="s3-access-key"
-      export TEST_S3_SECRET_KEY="s3-secret-key"
+      export TEST_S3_ACCESS_KEY="$MINIO_ACCESS_KEY"
+      export TEST_S3_SECRET_KEY="$MINIO_SECRET_KEY"
       export TEST_S3_BUCKET="test"
       export TEST_S3_REGION="ru"
 
